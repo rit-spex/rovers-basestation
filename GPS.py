1b2ea5e1f368573c202f2d23b2e68b677a039d73
Garrett59100 <garrett59100@gmail.com>
2025-02-07 21:17:06 -0500

    my ver of GPS cuz SOMEONE didnt upload their code

--- GPS.py
+++ GPS.py
@@ -1,3 +1,19 @@
-from pyftdi.ftdi import Ftdi
-# Ftdi().open_from_url('ftdi:///?')
-Ftdi.show_devices()
+import time
+import board
+import busio
+
+i2c = busio.I2C(board.SCL, board.SDA, 115200)
+
+devices = i2c.scan()
+if len(devices) == 0:
+    print("no i2c devices")
+    exit()
+
+device = devices[0]
+print(f"reading off device {device}")
+
+buffer = []
+while(True):
+    i2c.readfrom_into(device, buffer)
+    print(buffer)
+    time.sleep(0.5)
